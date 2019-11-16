5eb144923b537034b7b2161a6eeba68a14f0db9d
Nicholas Broad <nbroad94@gmail.com>
2019-11-16 14:25:26 -0500

    fix: local .env matches heroku env variables

--- config.py
+++ config.py
@@ -1,22 +1,17 @@
 import os
-import json
-
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-with open('info.json', 'r') as json_file:
-    user_info = json.load(json_file)
 
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'will-change-one-day'
-    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = 465
     MAIL_USE_SSL = True
     MAIL_USE_TLS = False
-    MAIL_USERNAME = user_info['gmail_username']
-    MAIL_PASSWORD = user_info['gmail_password']
-    ADMINS = user_info['admins']
+    MAIL_USERNAME = os.environ.get('USERNAME')
+    MAIL_PASSWORD = os.environ.get('PASSWORD')
+    ADMINS = os.environ.get('ADMIN')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
