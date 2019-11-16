adb3f3d582bfb4425807ac9a2d442256844329e3
Nicholas Broad <nbroad94@gmail.com>
2019-11-16 14:42:09 -0500

    fix: check for IN_HEROKU env variable

--- config.py
+++ config.py
@@ -4,14 +4,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    in_heroku = os.environ.get('IN_HEROKU', None)
+    if not in_heroku:
+        SECRET_KEY = 'will-change-one-day'
+        MAIL_SERVER = os.environ.get('MAIL_SERVER')
+        MAIL_PORT = 465
+        MAIL_USE_SSL = True
+        MAIL_USE_TLS = False
+        MAIL_USERNAME = os.environ.get('USERNAME')
+        MAIL_PASSWORD = os.environ.get('PASSWORD')
+        ADMINS = os.environ.get('ADMIN')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'will-change-one-day'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = 465
-    MAIL_USE_SSL = True
-    MAIL_USE_TLS = False
-    MAIL_USERNAME = os.environ.get('USERNAME')
-    MAIL_PASSWORD = os.environ.get('PASSWORD')
-    ADMINS = os.environ.get('ADMIN')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
