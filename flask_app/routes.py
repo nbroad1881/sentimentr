595b009be4b3abd065862cb0b279b1e3e89e09c6
Nicholas Broad <nbroad94@gmail.com>
2019-12-18 10:35:04 -0500

    feat: fast-ai lstm

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,17 +1,36 @@
 from datetime import datetime, timedelta
 import json
 
+from fastai.text import *
+
 from flask_app import app, db
 from flask import render_template, url_for, jsonify, request
 from flask_app.models import Article, Score
-from flask_app.forms import PostForm
+# from flask_app.forms import PostForm
 from flask_mail import Message
 from sentinews.models.vader import VaderAnalyzer
 from sentinews.models.textblob import TextBlobAnalyzer
+from sentinews.models.lstm import LSTMAnalyzer
 
 DEFAULT_TIME_BACK = timedelta(days=14)
 DEFAULT_DATA_POINTS_LIMIT = 50
 
+import os
+
+dirname = os.path.dirname(__file__)
+print(dirname)
+path = os.path.join(dirname, 'static/lstm_models/')
+# print(path)
+# lstm = load_learner(path, 'first_model.pkl')
+# print(str(lstm.predict('What a glorious day for our fine leader, Donald Trump')[2][1]))
+# print(lstm.predict('Elizabeth Warren rallies great support in Michigan'))
+# print(lstm.predict('Bernie Sanders is running a fading campaign'))
+
+models = {
+    'vader': VaderAnalyzer(),
+    'textblob': TextBlobAnalyzer(),
+    'lstm': LSTMAnalyzer(model_dir=path)
+}
 
 
 @app.route('/')
@@ -39,9 +58,11 @@ def news(agency):
     # For pages about news agencies
     return ''
 
-@app.route('/models/<model>')
+
+@app.route('/lstm_models/<model>')
 def model_page(model):
     return render_template('model.html', model=model)
+
 
 @app.route('/data/<model>', methods=['GET', 'POST'])
 def data(model):
@@ -49,36 +70,31 @@ def data(model):
     Finds the corresponding data requested for the given model.
     :return: JSON
     """
-    models = {
-        'vader': VaderAnalyzer(),
-        'textblob': TextBlobAnalyzer(),
-    }
+
     if request.method == 'POST':
-
-        post_data = request.data #specifying data range, news agency etc.
+        post_data = request.data  # specifying data range, news agency etc.
     if request.method == 'GET':
-
-    #     get default data
+        #     get default data
         pass
 
     return ''
 
+
 @app.route('/analyze/<model>', methods=['POST'])
 def analyze(model):
-    models = {
-        'vader': VaderAnalyzer(),
-        'textblob': TextBlobAnalyzer(),
-    }
+
     req = request.form['text']
 
     if model in models:
-        return jsonify(models[model].evaluate([req]))
+        return jsonify(models[model].evaluate(req))
+
 
 @app.route('/data_test')
 def data_test():
-    results = db.session.query(Article).order_by(Article.datetime.desc()).\
+    results = db.session.query(Article).order_by(Article.datetime.desc()). \
         limit(DEFAULT_DATA_POINTS_LIMIT).all()
-    return jsonify([r.to_dict()for r in results])
+    return jsonify([r.to_dict() for r in results])
+
 
 @app.route('/vader_data')
 def vader_data():
@@ -92,8 +108,8 @@ def vader_data():
     other_date = datetime.utcnow() - DEFAULT_TIME_BACK
 
     # filter(info.c.date.between(other_date, today)).\
-    results = db.session.query(info).\
-        order_by(info.c.date.desc()).\
+    results = db.session.query(info). \
+        order_by(info.c.date.desc()). \
         limit(DEFAULT_DATA_POINTS_LIMIT).all()
 
     return jsonify(results)
