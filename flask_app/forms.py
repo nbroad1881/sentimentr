92bccf4fe9cfdafb49a7fa9ad9aa0d318f423dbd
Nicholas Broad <nbroad94@gmail.com>
2020-01-14 13:13:45 -0500

    docs: add reminder to setup form for score requests from user

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 
+#todo: make a form for custom sentiment score requests
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
