6dd0b2689ec353691686ecc95cb73b73ab38911e
Nicholas Broad <nbroad94@gmail.com>
2020-01-14 13:13:16 -0500

    feat: add __repr__ method

--- flask_app/models.py
+++ flask_app/models.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from flask_app import db
 
 
@@ -46,35 +44,5 @@ class Article(db.Model):
             'lstm_p_neg': self.lstm_p_neg,
         }
 
-
-class Score(db.Model):
-    __tablename__ = 'scores'
-    url = db.Column(db.Text, primary_key=True)
-    vader_positive = db.Column(db.Float)
-    vader_negative = db.Column(db.Float)
-    vader_neutral = db.Column(db.Float)
-    vader_compound = db.Column(db.Float)
-    textblob_polarity = db.Column(db.Float)
-    textblob_subjectivity = db.Column(db.Float)
-    textblob_classification = db.Column(db.String(10))
-    textblob_p_pos = db.Column(db.Float)
-    textblob_p_neg = db.Column(db.Float)
-    lstm = db.Column(db.Float)
-
     def __repr__(self):
-        return f'{self.title} from {self.id}'
-
-    def __dict__(self):
-        return {
-            'url': self.url,
-            'vader_positive': self.vader_positive,
-            'vader_negative': self.vader_negative,
-            'vader_neutral': self.vader_neutral,
-            'vader_compound': self.vader_compound,
-            'textblob_polarity': self.textblob_polarity,
-            'textblob_subjectivity': self.textblob_subjectivity,
-            'textblob_classification': self.textblob_classification,
-            'textblob_p_pos': self.textblob_p_pos,
-            'textblob_p_neg': self.textblob_p_neg,
-            'lstm': self.lstm
-        }
+        return self.to_dict()
