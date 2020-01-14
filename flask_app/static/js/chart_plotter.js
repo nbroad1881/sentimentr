a090d3ab0c3d0e1423ff0420333cff61379be519
Nicholas Broad <nbroad94@gmail.com>
2020-01-14 13:16:59 -0500

    refactor: more global constants to improve readability
    rename createDatasets -> createDataset as it is only creating a single dataset
    rename set_arrays -> set_variables
    
    condense updateChart()

--- flask_app/static/js/chart_plotter.js
+++ flask_app/static/js/chart_plotter.js
@@ -1,31 +1,58 @@
-const NOCHANGE = 0;
 const TEXTBLOB = 1;
 const VADER = 2;
 const LSTM = 3;
 const BERT = 4;
-const ALL_MODELS = 9;
+const ALL_MODELS = 100;
 
-const TEXTBLOB_COLOR = "rgba(192, 57, 43, 1)";
-const VADER_COLOR = "rgba(104, 192, 43, 1)";
-const LSTM_COLOR = "rgba(43, 178, 192, 1)";
-const BERT_COLOR = "rgba(131, 43, 192, 1)";
+
+const CLASSIFIER_TITLE = {};
+CLASSIFIER_TITLE[TEXTBLOB] = 'TextBlob Scores';
+CLASSIFIER_TITLE[VADER] = 'VADER Scores';
+CLASSIFIER_TITLE[LSTM] = 'LSTM Scores';
+CLASSIFIER_TITLE[ALL_MODELS] = 'All Models';
+
+const CNN = 'CNN';
+const NYT = "New York Times";
+const FOX = 'Fox News';
+const ALL_NEWS_GROUPS = 'all_groups';
+
+const POINT_STYLES = {};
+POINT_STYLES[ALL_NEWS_GROUPS] = 'rectRot';
+POINT_STYLES[CNN] = 'circle';
+POINT_STYLES[NYT] = 'rect';
+POINT_STYLES[FOX] = 'triangle';
+
+const NEWS_COMPANIES = {};
+NEWS_COMPANIES[ALL_NEWS_GROUPS] = '';
+NEWS_COMPANIES[CNN] = 'CNN';
+NEWS_COMPANIES[NYT] = "New York Times";
+NEWS_COMPANIES[FOX] = 'Fox News';
+
+const LINE_COLORS = {};
+LINE_COLORS[ALL_NEWS_GROUPS] = "rgba(192, 57, 43, 1)";
+LINE_COLORS[TEXTBLOB] = "rgba(192, 57, 43, 1)";
+LINE_COLORS[VADER] = "rgba(104, 192, 43, 1)";
+LINE_COLORS[LSTM] = "rgba(43, 178, 192, 1)";
+LINE_COLORS[BERT] = "rgba(131, 43, 192, 1)";
 
 const DEFAULT_NUM_DAYS = 180;
 
 let getData = $.get('/data/');
-
-let getLSTMData = $.post('/analyze/lstm', {text: 'Great win for Bernie Sanders'})
+let getLSTMData = $.post('/analyze/lstm', {text: 'Great win for Bernie Sanders'});
 let classifiers = {};
+classifiers[TEXTBLOB] = {};
+classifiers[VADER] = {};
+classifiers[LSTM] = {};
 let titles = [];
 let classifier_labels = [];
 let news_companies = [];
 let chart;
 let dates = [];
-let dates_averaged = [];
 let first_index = 0;
 let second_index = -1;
 let current_classifier = TEXTBLOB;
-
+let current_news = ALL_NEWS_GROUPS;
+let second_news_index;
 
 
 $("#text-button").click(function () {
@@ -43,11 +70,11 @@ getLSTMData.done(function (results) {
 
 getData.done(function (results) {
     const ctx = document.getElementById('myChart').getContext('2d');
-    set_arrays(results, false);
+    set_variables(results);
 
     const data = {
         labels: classifier_labels,
-        datasets: createDatasets(TEXTBLOB)
+        datasets: createDataset(TEXTBLOB, ALL_NEWS_GROUPS)
     };
     const options = {
         type: 'line',
@@ -118,78 +145,42 @@ function roundToTwo(num) {
     return +(Math.round(num + "e+3") + "e-3");
 }
 
-//todo: make scales correct, add lstm
-function createDatasets(chartIdentifier) {
-    let new_label;
-    let new_color;
-    let new_fill = false;
-    let new_data;
-
-    switch (chartIdentifier) {
-        case (TEXTBLOB):
-            new_label = 'TextBlob Scores';
-            new_color = TEXTBLOB_COLOR;
-            break;
-        case (VADER):
-            new_label = 'Vader Scores';
-            new_color = VADER_COLOR;
-            break;
-        case (LSTM):
-            new_label = 'LSTM Scores';
-            new_color = LSTM_COLOR;
-            break;
-
-        case (BERT):
-            new_color = BERT_COLOR;
-            break;
-        case (ALL_MODELS):
-            return [createDatasets(TEXTBLOB)[0], createDatasets(VADER)[0], createDatasets(LSTM)[0]];
-        case(NOCHANGE):
-            return createDatasets(current_classifier);
+function createDataset(chartIdentifier, news_co) {
+    if(chartIdentifier === ALL_MODELS){
+        return [createDataset(TEXTBLOB, news_co)[0], createDataset(VADER, news_co)[0], createDataset(LSTM, news_co)[0]];
     }
-    if (chartIdentifier !== NOCHANGE){
-        new_data = classifiers[chartIdentifier];
-    }
-
+    let styles = get_line_style(chartIdentifier, news_co);
+    console.log(classifiers);
+    console.log(current_classifier, current_news);
+    console.log(chartIdentifier, news_co);
+    console.log(classifiers[chartIdentifier]);
+    let new_data = classifiers[chartIdentifier][news_co];
     return [{
-        label: new_label,
-        fill: new_fill,
-        borderColor: new_color,
-        backgroundColor: new_color,
+        label: styles['label'],
+        fill: false,
+        borderColor: styles['color'],
+        backgroundColor: 'rgba(255,255,255,1)',
+        pointStyle: styles['point'],
+        pointRadius:0,
+        pointBorderWidth:4,
+        borderWidth:2,
+        tension:0.2,
         data: new_data.slice(first_index, second_index)
     }];
 }
 
-function updateChart(chartIdentifier) {
+function updateChart(chartIdentifier, news_co) {
     chart.options.legend.display = false;
-    switch (chartIdentifier) {
-        case(TEXTBLOB):
-            chart.options.title.text = 'TextBlob Scores';
-            current_classifier = TEXTBLOB;
-            break;
-        case(VADER):
-            chart.options.title.text = 'Vader Scores';
-            current_classifier = VADER;
-            break;
-        case(LSTM):
-            chart.options.title.text = 'LSTM Scores';
-            current_classifier = LSTM;
-            break;
-        case(ALL_MODELS):
-            chart.options.title.text = 'All Models';
-            chart.options.legend.display = true;
-            current_classifier = ALL_MODELS;
-            break;
-        case(NOCHANGE):
-            if (chart.data.datasets.length > 1) {
-                chart.options.legend.display = true;
-            }
-            break;
-        default:
-            break;
+    chart.options.title.text = CLASSIFIER_TITLE[chartIdentifier];
+    current_classifier = chartIdentifier;
+    current_news = news_co;
+
+    if (chartIdentifier === ALL_MODELS || news_co !== ALL_NEWS_GROUPS) {
+        chart.options.legend.display = true;
     }
+
     chart.data.labels = classifier_labels.slice(first_index, second_index);
-    chart.data.datasets = createDatasets(chartIdentifier);
+    chart.data.datasets = createDataset(chartIdentifier, news_co);
     chart.update();
 }
 
@@ -197,41 +188,43 @@ function updateChart(chartIdentifier) {
 //First element is new label
 //Second element is new border dash style
 function get_line_style(chartIdentifier, news_co) {
-    if ([TEXTBLOB, VADER, LSTM, ALL_MODELS].includes(chartIdentifier)) {
-        if ([CNN, NYT, FOX].includes(news_co)) {
-            return {
-                new_label: NEWS_COMPANIES[news_co] + ' ' + CLASSIFIER_LABELS[chartIdentifier],
-                new_border_dash: DASH_STYLES[news_co];
-            }
-        }
-        return [CLASSIFIER_LABELS[chartIdentifier], DASH_STYLES[DEFAULT]]
+    if (news_co !== null) {
+        return {
+            label: NEWS_COMPANIES[news_co] + ' ' + CLASSIFIER_TITLE[chartIdentifier],
+            color: LINE_COLORS[chartIdentifier],
+            point: POINT_STYLES[news_co],
+        };
     }
-    return null;
+    return {
+        label: CLASSIFIER_TITLE[chartIdentifier],
+        color: LINE_COLORS[chartIdentifier],
+        point: POINT_STYLES[ALL_NEWS_GROUPS],
+    };
 }
 
 
 $("#textblob").click(function () {
     console.log('1');
-    updateChart(TEXTBLOB);
+    updateChart(TEXTBLOB, current_news);
 });
 $("#vader").click(function () {
     console.log('2');
-    updateChart(VADER);
+    updateChart(VADER, current_news);
 });
 $("#lstm").click(function () {
     console.log('3');
-    updateChart(LSTM);
+    updateChart(LSTM, current_news);
 });
 $("#all-models").click(function () {
     updateChart(ALL_MODELS);
 });
 
 
-$(".period").click(function () {
-    let period = this.id;
+$(".filter-btn").click(function () {
+    let filter_btn = this.id;
     let indices = [0, dates.length];
     //todo: change starting date to be custom
-    switch (period) {
+    switch (filter_btn) {
         case "1w":
             indices = get_date_indices(new Date(), dates, 7);
             break;
@@ -253,20 +246,34 @@ $(".period").click(function () {
     if (indices[0] !== -1) {
         first_index = indices[0];
         second_index = indices[1];
-        updateChart(NOCHANGE);
+        if (second_news_index === -1) {
+            second_news_index = second_index;
+        }
+        updateChart(current_classifier, current_news);
     }
 
 });
 
-function set_arrays(results) {
+function set_variables(results) {
+    console.log(results);
     titles = results['titles'];
     news_companies = results['news_co'];
-    start_date = new Date();
+    classifiers[TEXTBLOB][ALL_NEWS_GROUPS] = results['textblob_averaged'];
+    classifiers[TEXTBLOB][CNN] = results['cnn_textblob_averaged'];
+    classifiers[TEXTBLOB][NYT] = results['nyt_textblob_averaged'];
+    classifiers[TEXTBLOB][FOX] = results['fox_textblob_averaged'];
+
+    classifiers[VADER][ALL_NEWS_GROUPS] = results['vader_averaged'];
+    classifiers[VADER][CNN] = results['cnn_vader_averaged'];
+    classifiers[VADER][NYT] = results['nyt_vader_averaged'];
+    classifiers[VADER][FOX] = results['fox_vader_averaged'];
+
+    classifiers[LSTM][ALL_NEWS_GROUPS] = results['lstm_averaged'];
+    classifiers[LSTM][CNN] = results['cnn_lstm_averaged'];
+    classifiers[LSTM][NYT] = results['nyt_lstm_averaged'];
+    classifiers[LSTM][FOX] = results['fox_lstm_averaged'];
+
     dates = make_date_list(new Date(), DEFAULT_NUM_DAYS);
-    dates_averaged = results['dates_array'];
-    classifiers[TEXTBLOB] = results['textblob_averaged'];
-    classifiers[VADER] = results['vader_averaged'];
-    classifiers[LSTM] = results['lstm_averaged'];
     classifier_labels = dates;
 }
 
@@ -285,8 +292,6 @@ function make_date_list(starting_date, number_of_days) {
     return date_array;
 }
 
-// # todo: put zeros in the array
-
 function get_date_indices(starting_date, date_array, num_days) {
     let first_index = -1;
     let second_index = date_array.length;
@@ -302,7 +307,7 @@ function get_date_indices(starting_date, date_array, num_days) {
         }
     }
     if (first_index !== -1) {
-        last_date = date_array[date_array.length-1];
+        last_date = date_array[date_array.length - 1];
         //if last date is within range of requested days
         if (last_date < second_date) {
             second_index = first_index + num_days;
