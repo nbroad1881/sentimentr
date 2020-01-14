bf6d015fab879a05de1e754e3d40c8a0277c1985
Nicholas Broad <nbroad94@gmail.com>
2020-01-14 13:10:16 -0500

    feat: add celery functionality. test with find_new_articles

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,53 +1,45 @@
-import os
 import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+# todo: utilize migrate
 from flask_migrate import Migrate
-from flask_mail import Mail
+from celery import Celery
+from sentinews.scraping.newsapi_scraper import NewsAPIScraper
+
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+
+
 migrate = Migrate(app, db)
 
-# mail = Mail(app)
 
-# if not app.debug:
-#     if app.config['MAIL_SERVER']:
-#         auth = None
-#         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-#             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-#         secure = None
-#         if app.config['MAIL_USE_TLS']:
-#             secure = ()
-#         mail_handler = SMTPHandler(
-#             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-#             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-#             toaddrs=app.config['ADMINS'], subject='Flask App Failure',
-#             credentials=auth, secure=secure)
-#         mail_handler.setLevel(logging.ERROR)
-#         app.logger.addHandler(mail_handler)
-#
-# if app.config['LOG_TO_STDOUT']:
-#     stream_handler = logging.StreamHandler()
-#     stream_handler.setLevel(logging.INFO)
-#     app.logger.addHandler(stream_handler)
-# else:
-#     if not os.path.exists('logs'):
-#         os.mkdir('logs')
-#     file_handler = RotatingFileHandler('logs/flask_app.log', maxBytes=10240,
-#                                        backupCount=10)
-#     file_handler.setFormatter(logging.Formatter(
-#         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-#     file_handler.setLevel(logging.INFO)
-#     app.logger.addHandler(file_handler)
-#
-#     app.logger.setLevel(logging.INFO)
-#     app.logger.info('Flask app startup')
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
 
-from flask_app import routes, models, errors
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
 
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(app)
+napi = NewsAPIScraper(limited=True)
+
+@celery.task(ignore_result=True)
+def find_new_articles():
+    napi.get_titles()
+    napi.db.close_session()
+
+from flask_app import routes, models
