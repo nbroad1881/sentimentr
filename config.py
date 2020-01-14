1714bd8e9f45200145dc4d10e13bf46d95fb3408
Nicholas Broad <nbroad94@gmail.com>
2020-01-14 13:09:28 -0500

    feat: add database/rabbitmq/celery variables from .env file

--- config.py
+++ config.py
@@ -1,26 +1,37 @@
 import os
+import json
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+DB_TABLE_NAME = os.environ.get('DB_TABLE_NAME')
+DB_SELECT_COLUMNS = json.loads(os.environ.get('DB_SELECT_COLUMNS'))
+DB_PARSE_COLUMNS = json.loads(os.environ.get('DB_PARSE_COLUMNS'))
+DB_DATETIME_COLUMN_NAME = os.environ.get('DB_DATETIME_COLUMN_NAME')
+
+DB_ENDPOINT = os.environ.get('DB_ENDPOINT')
+DB_USERNAME = os.environ.get('DB_USERNAME')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_NAME = os.environ.get('DB_NAME')
+DB_PORT = os.environ.get('DB_PORT')
+DB_URI = f"postgres://{DB_USERNAME}:{DB_PASSWORD}@{DB_ENDPOINT}:{DB_PORT}/{DB_NAME}"
 
 class Config(object):
     in_dev = os.environ.get('IN_DEV', None)
 
-    ENDPOINT = os.environ.get('ENDPOINT')
-    PORT = os.environ.get('PORT')
-    USER = os.environ.get('USERNAME')
-    PW = os.environ.get('PASSWORD')
-    DBNAME = os.environ.get('DBNAME')
-    SQLALCHEMY_DATABASE_URI = f"postgres://{USER}:{PW}@{ENDPOINT}:{PORT}/{DBNAME}"
+    RABBITMQ_HOST = '0.0.0.0'
+    RABBITMQ_PORT = '5672'
+    CELERY_BROKER_URL = os.environ.get('RABBITMQ_URL', f"amqp://guest@{RABBITMQ_HOST}:{RABBITMQ_PORT}//")
+    CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+
+    SQLALCHEMY_DATABASE_URI = DB_URI
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'will-change-one-day'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = 465
-    MAIL_USE_SSL = True
-    MAIL_USE_TLS = False
-    MAIL_USERNAME = os.environ.get('USERNAME')
-    MAIL_PASSWORD = os.environ.get('PASSWORD')
     ADMINS = os.environ.get('ADMIN')
+
+    DB_SELECT_COLUMNS = os.environ.get('DB_SELECT_COLUMNS')
 
 
