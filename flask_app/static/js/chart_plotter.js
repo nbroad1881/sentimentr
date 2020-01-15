c0c768ed207d10bb6dde0116a3369e3346f50c8f
Nicholas Broad <nbroad94@gmail.com>
2020-01-15 16:42:19 -0500

    refactor: handle new form from db side, have simple interactive graph

--- flask_app/static/js/chart_plotter.js
+++ flask_app/static/js/chart_plotter.js
@@ -1,32 +1,27 @@
-const TEXTBLOB = 1;
-const VADER = 2;
-const LSTM = 3;
-const BERT = 4;
-const ALL_MODELS = 100;
+const TEXTBLOB = 'textblob';
+const VADER = 'vader';
+const LSTM = 'lstm';
+const BERT = 'bert';
+const ALL_MODELS = 'all models';
 
 
-const CLASSIFIER_TITLE = {};
-CLASSIFIER_TITLE[TEXTBLOB] = 'TextBlob Scores';
-CLASSIFIER_TITLE[VADER] = 'VADER Scores';
-CLASSIFIER_TITLE[LSTM] = 'LSTM Scores';
-CLASSIFIER_TITLE[ALL_MODELS] = 'All Models';
+const MODELS = [TEXTBLOB, VADER, LSTM, BERT, ALL_MODELS];
 
 const CNN = 'CNN';
-const NYT = "New York Times";
+const NYT = "The New York Times";
 const FOX = 'Fox News';
-const ALL_NEWS_GROUPS = 'all_groups';
+const ALL_NEWS_GROUPS = 'all news';
 
-const POINT_STYLES = {};
-POINT_STYLES[ALL_NEWS_GROUPS] = 'rectRot';
-POINT_STYLES[CNN] = 'circle';
-POINT_STYLES[NYT] = 'rect';
-POINT_STYLES[FOX] = 'triangle';
+const NEWS_COMPANIES = [CNN, NYT, FOX];
 
-const NEWS_COMPANIES = {};
-NEWS_COMPANIES[ALL_NEWS_GROUPS] = '';
-NEWS_COMPANIES[CNN] = 'CNN';
-NEWS_COMPANIES[NYT] = "New York Times";
-NEWS_COMPANIES[FOX] = 'Fox News';
+const TRUMP = 'trump';
+const BIDEN = 'biden';
+const WARREN = 'warren';
+const SANDERS = 'sanders';
+const HARRIS = 'harris';
+const BUTTIGIEG = 'buttigieg';
+const CANDIDATES = [TRUMP, BIDEN, WARREN, HARRIS, SANDERS, BUTTIGIEG];
+
 
 const LINE_COLORS = {};
 LINE_COLORS[ALL_NEWS_GROUPS] = "rgba(192, 57, 43, 1)";
@@ -37,7 +32,101 @@ LINE_COLORS[BERT] = "rgba(131, 43, 192, 1)";
 
 const DEFAULT_NUM_DAYS = 180;
 
-let getData = $.get('/data/');
+const CNN_KEY = 'cnn';
+const FOX_KEY = 'fox news';
+const NYT_KEY = 'the new york times';
+
+const NEWS_KEYS = [CNN_KEY, FOX_KEY, NYT_KEY];
+
+const DATETIME_KEY = 'datetime';
+const LSTM_SCORE_KEY = 'lstm_score';
+const TEXTBLOB_SCORE_KEY = 'textblob_score';
+const VADER_SCORE_KEY = 'vader_score';
+const NEWS_CO_KEY = 'news_co';
+const URL_KEY = 'url';
+const TITLE_KEY = 'title';
+
+// const LABEL_KEY = {};
+// LABEL_KEY[CNN_KEY] = 'CNN';
+// LABEL_KEY[FOX_KEY] = 'Fox News';
+// LABEL_KEY[NYT_KEY] = 'The New York Times';
+// LABEL_KEY[LSTM_SCORE_KEY] = 'LSTM';
+// LABEL_KEY[TEXTBLOB_SCORE_KEY] = 'TextBlob';
+// LABEL_KEY[VADER_SCORE_KEY] = 'VADER';
+
+const LABEL_TO_KEY = {};
+LABEL_TO_KEY['TextBlob'] = TEXTBLOB_SCORE_KEY;
+LABEL_TO_KEY['VADER'] = VADER_SCORE_KEY;
+LABEL_TO_KEY['LSTM'] = LSTM_SCORE_KEY;
+LABEL_TO_KEY['CNN'] = CNN_KEY;
+LABEL_TO_KEY['Fox-News'] = FOX_KEY;
+LABEL_TO_KEY['The-New-York-Times'] = NYT_KEY;
+
+
+
+
+const NEWS_COMPANY_KEYS = [CNN_KEY, FOX_KEY, NYT_KEY];
+const MODEL_KEYS = [LSTM_SCORE_KEY, TEXTBLOB_SCORE_KEY, VADER_SCORE_KEY];
+
+
+let plotting_data = {};
+
+
+const ctx = document.getElementById('myChart').getContext('2d');
+let options = {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{}],
+    },
+    scales: {
+        xAxes: [{
+            type: 'time',
+            display: true,
+            time: {
+                unit: 'day',
+            },
+            scaleLabel: {
+                display: true,
+                labelString: 'Date'
+            }
+        }],
+    },
+    options: {
+        tooltips:{
+            callbacks:{
+                // todo: figure out how to put title of article for tooltip
+                // title: function(tooltipItem, data){
+                //     return data.datasets[tooltipItem.datasetIndex].titles[tooltipItem['index']] || '';
+                // },
+                label: function(tooltipItem, data){
+                    return data.datasets[tooltipItem.datasetIndex].news_co || '';
+                },
+                afterLabel: function(tooltipItem, data){
+                    return data.labels[tooltipItem['index']]
+                }
+            }
+        },
+        title: {
+            display: true,
+            text: 'plz work',
+            fontSize: 30
+
+        },
+        legend: {
+            display: false,
+        },
+    }
+
+
+};
+let chart = new Chart(ctx, options);
+
+
+// let getData = $.get('/data/');
+// let allData = $.get('/data/');
+let candidateData = $.get('/candidate/all');
+let newsData = $.get('/news_company/all');
 let getLSTMData = $.post('/analyze/lstm', {text: 'Great win for Bernie Sanders'});
 let classifiers = {};
 classifiers[TEXTBLOB] = {};
@@ -46,13 +135,12 @@ classifiers[LSTM] = {};
 let titles = [];
 let classifier_labels = [];
 let news_companies = [];
-let chart;
 let dates = [];
 let first_index = 0;
 let second_index = -1;
 let current_classifier = TEXTBLOB;
 let current_news = ALL_NEWS_GROUPS;
-let second_news_index;
+let currentFocus = 'candidates';
 
 
 $("#text-button").click(function () {
@@ -67,86 +155,96 @@ getLSTMData.done(function (results) {
     console.log(results)
 });
 
-
-getData.done(function (results) {
-    const ctx = document.getElementById('myChart').getContext('2d');
-    set_variables(results);
-
-    const data = {
-        labels: classifier_labels,
-        datasets: createDataset(TEXTBLOB, ALL_NEWS_GROUPS)
-    };
-    const options = {
-        type: 'line',
-
-        data: data,
-        aspectRatio: 2,
-
-        options: {
-            tooltips: {
-                callbacks: {
-                    title: function (tooltipItems) {
-                        let index = tooltipItems[0].index;
-                        let news_co = news_companies[index];
-                        let score = tooltipItems[0].value;
-                        let date = classifier_labels[index];
-                        let minutes = date.getUTCMinutes();
-                        if (minutes < 10) {
-                            minutes = '0' + String(minutes);
-                        }
-                        let hour_min = date.getUTCHours() + ':' + minutes;
-
-                        return roundToTwo(score) + ' - ' + news_co + ' - ' + date.toDateString() + ' - ' + hour_min;
-                    },
-
-                    label: function (tooltipItem) {
-
-                        return titles[tooltipItem.index];
-                    }
-                },
-                titleFontSize: 20,
-                bodyFontSize: 16,
-
-            },
-            scales: {
-                xAxes: [{
-                    type: 'time',
-                    display: true,
-                    time: {
-                        unit: 'day',
-                        //     displayFormats: {
-                        //         day: 'MMM Do HA'
-                        // }
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Date'
-                    }
-                }],
-            },
-            title: {
-                display: true,
-                text: 'TextBlob Scores',
-                fontSize: 30
-            },
-            legend: {
-                display: false,
-                labels: {
-                    fontSize: 18
-                }
-            }
-        }
-    };
-    chart = new Chart(ctx, options);
-
+candidateData.done(function (results) {
+    console.log(results);
+    CANDIDATES.forEach(function (c) {
+        plotting_data[c] = {};
+        NEWS_KEYS.forEach(function (n) {
+            plotting_data[c][n] = results[c][n];
+        });
+    });
+    console.log(plotting_data);
+    setDatasets();
+    showDataset(BIDEN, CNN_KEY, TEXTBLOB_SCORE_KEY, false);
+    chart.update()
 });
+
+// newsData.done(function (results) {
+//     plotting_data[CNN] = results[CNN_KEY];
+//     plotting_data[NYT] = results[NYT_KEY];
+//     plotting_data[FOX] = results[FOX_KEY];
+// });
+
 
 function roundToTwo(num) {
     return +(Math.round(num + "e+3") + "e-3");
 }
 
+function setLabels(candidate, news_co_key, model_key) {
+    chart.data.labels = plotting_data[candidate][news_co_key][DATETIME_KEY];
+}
+
+function showDataset(candidate, news_co, model, toggle) {
+    news_co = news_co.replace('-', ' ');
+    model = model.replace('-', ' ');
+    setLabels(candidate, news_co, model);
+
+    chart.data.datasets.forEach(ds => {
+        if (ds.label === candidate + '-' + news_co + '-' + model) {
+            if (toggle) {
+                ds['hidden'] = !ds['hidden'];
+            } else {
+                ds.hidden = false;
+            }
+        }
+    });
+}
+
+function setDatasets() {
+    let new_datasets = [];
+
+    CANDIDATES.forEach(c => {
+        NEWS_COMPANY_KEYS.forEach(n => {
+            MODEL_KEYS.forEach(m => {
+                new_datasets.push({
+                    label: c + '-' + n + '-' + m,
+                    data: plotting_data[c][n][m],
+                    borderColor: "rgba(192, 57, 43, 1)",
+                    hidden: true,
+                    fill: false,
+                    titles: plotting_data[c][n]['title'],
+                    news_co: n,
+                });
+            });
+
+        });
+    });
+
+    //     [{
+    //     label: LABEL_KEY[LSTM_SCORE_KEY],
+    //     data: plotting_data[candidate][news_co][LSTM_SCORE_KEY],
+    //     borderColor: LINE_COLORS[LSTM],
+    //     hidden:false,
+    //     fill:false,
+    // },{
+    //     label: LABEL_KEY[TEXTBLOB_SCORE_KEY],
+    //     data: plotting_data[candidate][news_co][TEXTBLOB_SCORE_KEY],
+    //     borderColor: LINE_COLORS[TEXTBLOB],
+    //     hidden:false,
+    //     fill:false,
+    // },{
+    //     label: LABEL_KEY[VADER_SCORE_KEY],
+    //     data: plotting_data[candidate][news_co][VADER_SCORE_KEY],
+    //     borderColor: LINE_COLORS[VADER],
+    //     hidden:false,
+    //     fill:false,
+    // }];
+
+    chart.data.datasets = new_datasets;
+}
+
 function createDataset(chartIdentifier, news_co) {
-    if(chartIdentifier === ALL_MODELS){
+    if (chartIdentifier === ALL_MODELS) {
         return [createDataset(TEXTBLOB, news_co)[0], createDataset(VADER, news_co)[0], createDataset(LSTM, news_co)[0]];
     }
     let styles = get_line_style(chartIdentifier, news_co);
@@ -161,143 +259,104 @@ function createDataset(chartIdentifier, news_co) {
         borderColor: styles['color'],
         backgroundColor: 'rgba(255,255,255,1)',
         pointStyle: styles['point'],
-        pointRadius:0,
-        pointBorderWidth:4,
-        borderWidth:2,
-        tension:0.2,
+        pointRadius: 0,
+        pointBorderWidth: 4,
+        borderWidth: 2,
+        tension: 0.2,
         data: new_data.slice(first_index, second_index)
     }];
 }
 
-function updateChart(chartIdentifier, news_co) {
-    chart.options.legend.display = false;
-    chart.options.title.text = CLASSIFIER_TITLE[chartIdentifier];
-    current_classifier = chartIdentifier;
-    current_news = news_co;
 
-    if (chartIdentifier === ALL_MODELS || news_co !== ALL_NEWS_GROUPS) {
-        chart.options.legend.display = true;
+function toggleView(btn_id) {
+    chart.data.datasets.forEach(ds =>{
+        ds.hidden = true;
+    });
+
+    let active_btns = document.querySelectorAll('.selected-feature');
+    let model;
+    let candidate;
+    let news_co;
+
+    for (let i=0; i < active_btns.length; i++){
+        if(active_btns[i].classList.contains('model-btn')){
+            model = LABEL_TO_KEY[active_btns[i].id];
+        }
+        else if(active_btns[i].classList.contains('candidate-btn')){
+            candidate = active_btns[i].id;
+        }
+        else if(active_btns[i].classList.contains('news-btn')){
+            news_co = LABEL_TO_KEY[active_btns[i].id];
+        }
     }
 
-    chart.data.labels = classifier_labels.slice(first_index, second_index);
-    chart.data.datasets = createDataset(chartIdentifier, news_co);
+    showDataset(candidate, news_co, model, false);
+
+    // let key = btn_id.replace('-', ' '); // html id has dashes, these keys have spaces
+    // if (key.includes('all')) {
+    //     chart.data.datasets.forEach((ds) => {
+    //         ds['hidden'] = false;
+    //     });
+    // } else {
+    //     chart.data.datasets.forEach((ds) => {
+    //         if (ds['label'] === key) {
+    //             ds['hidden'] = !ds['hidden'];
+    //         }
+    //     });
+    // }
+
+    // else if(CANDIDATES.includes(key)){
+    //
+    // }else if(NEWS_COMPANIES.includes(key)){
+    //
+    // }
+
     chart.update();
 }
 
-//Returns a two element array.
-//First element is new label
-//Second element is new border dash style
-function get_line_style(chartIdentifier, news_co) {
-    if (news_co !== null) {
-        return {
-            label: NEWS_COMPANIES[news_co] + ' ' + CLASSIFIER_TITLE[chartIdentifier],
-            color: LINE_COLORS[chartIdentifier],
-            point: POINT_STYLES[news_co],
-        };
+let primary_button_ids = ['TextBlob', 'LSTM', 'VADER', 'all-models'];
+let dark_button_ids = ['trump', 'biden', 'warren', 'harris', 'sanders', 'buttigieg'];
+let success_button_ids = ['CNN', 'The-New-York-Times', 'Fox-News'];
+
+function modifyButtons(btn_id) {
+    let highlight = "btn-";
+    let outline = "btn-outline-";
+    let parentSelector = "";
+    if (primary_button_ids.includes(btn_id)) {
+        highlight += 'primary';
+        outline += 'primary';
+        parentSelector = '#model-selector';
+    } else if (dark_button_ids.includes(btn_id)) {
+        highlight += 'dark';
+        outline += 'dark';
+        parentSelector = '#candidate-selector';
+    } else if (success_button_ids.includes(btn_id)) {
+        highlight += 'success';
+        outline += 'success';
+        parentSelector = '#news-co-selector';
     }
-    return {
-        label: CLASSIFIER_TITLE[chartIdentifier],
-        color: LINE_COLORS[chartIdentifier],
-        point: POINT_STYLES[ALL_NEWS_GROUPS],
-    };
+
+    let buttons = document.querySelector(parentSelector).children;
+    let add = [outline];
+    let remove = [highlight, 'selected-feature'];
+    for (let i = 0; i < buttons.length; i++) {
+        add.forEach(a => {
+            buttons[i].classList.add(a);
+        });
+        remove.forEach(r => {
+            buttons[i].classList.remove(r);
+        });
+    }
+    let btn = document.querySelector('#' + btn_id);
+    btn.classList.add(highlight);
+    btn.classList.add('selected-feature');
+    btn.classList.remove(outline);
 }
-
-
-$("#textblob").click(function () {
-    console.log('1');
-    updateChart(TEXTBLOB, current_news);
-});
-$("#vader").click(function () {
-    console.log('2');
-    updateChart(VADER, current_news);
-});
-$("#lstm").click(function () {
-    console.log('3');
-    updateChart(LSTM, current_news);
-});
-$("#all-models").click(function () {
-    updateChart(ALL_MODELS, current_news);
-});
-
-$(".news-btn").click(function () {
-    let news_btn = this.id;
-    switch (news_btn) {
-        case "cnn":
-            updateChart(current_classifier, CNN);
-            console.log("*******************************");
-            console.log(current_classifier, current_news, CNN);
-            break;
-        case "nyt":
-            updateChart(current_classifier, NYT);
-            break;
-        case "fox":
-            updateChart(current_classifier, FOX);
-            break;
-        case "all-news":
-            updateChart(current_classifier, ALL_NEWS_GROUPS);
-            break;
-        default:
-            break;
-    }
-});
-
 
 $(".filter-btn").click(function () {
-    let filter_btn = this.id;
-    let indices = [0, dates.length];
-    //todo: change starting date to be custom
-    switch (filter_btn) {
-        case "1w":
-            indices = get_date_indices(new Date(), dates, 7);
-            break;
-        case "2w":
-            indices = get_date_indices(new Date(), dates, 14);
-            break;
-        case "1m":
-            indices = get_date_indices(new Date(), dates, 30);
-            break;
-        case "3m":
-            indices = get_date_indices(new Date(), dates, 90);
-            break;
-        case "6m":
-            indices = get_date_indices(new Date(), dates, 180);
-            break;
-        default:
-            break;
-    }
-    if (indices[0] !== -1) {
-        first_index = indices[0];
-        second_index = indices[1];
-        if (second_news_index === -1) {
-            second_news_index = second_index;
-        }
-        updateChart(current_classifier, current_news);
-    }
-
+    modifyButtons(this.id);
+    toggleView(this.id);
 });
-
-function set_variables(results) {
-    console.log(results);
-    titles = results['titles'];
-    news_companies = results['news_co'];
-    classifiers[TEXTBLOB][ALL_NEWS_GROUPS] = results['textblob_averaged'];
-    classifiers[TEXTBLOB][CNN] = results['cnn_textblob_averaged'];
-    classifiers[TEXTBLOB][NYT] = results['nyt_textblob_averaged'];
-    classifiers[TEXTBLOB][FOX] = results['fox_textblob_averaged'];
-
-    classifiers[VADER][ALL_NEWS_GROUPS] = results['vader_averaged'];
-    classifiers[VADER][CNN] = results['cnn_vader_averaged'];
-    classifiers[VADER][NYT] = results['nyt_vader_averaged'];
-    classifiers[VADER][FOX] = results['fox_vader_averaged'];
-
-    classifiers[LSTM][ALL_NEWS_GROUPS] = results['lstm_averaged'];
-    classifiers[LSTM][CNN] = results['cnn_lstm_averaged'];
-    classifiers[LSTM][NYT] = results['nyt_lstm_averaged'];
-    classifiers[LSTM][FOX] = results['fox_lstm_averaged'];
-
-    dates = make_date_list(new Date(), DEFAULT_NUM_DAYS);
-    classifier_labels = dates;
-}
 
 
 //Starting date should be a Date object
