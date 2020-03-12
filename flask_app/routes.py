44f87be8b1e10d0bfa2e212b0d318e47425dae47
Nicholas Broad <nbroad94@gmail.com>
2020-03-11 22:40:53 -0400

    feat: new routes: avg, join, rolling_average

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,14 +1,15 @@
 import os
 import logging
+from datetime import datetime as dt, timezone, timedelta
 
 from dateutil.parser import isoparse
-from flask import request, render_template
+from flask import request, render_template, jsonify
 from dotenv import load_dotenv
 from sqlalchemy.exc import IntegrityError
 
 from flask_app import app, db
-from flask_app.db_models import DBArticle, COLUMN_NAMES
-from flask_app.schemas import ArticleSchema
+from flask_app.db_models import DBArticle, DBScore, COLUMN_NAMES, Weekly
+from flask_app.schemas import ArticleSchema, ScoreSchema, WeeklySchema
 from sentinews.models import (VaderAnalyzer,
                               LSTMAnalyzer,
                               TextBlobAnalyzer,
@@ -16,6 +17,10 @@ from sentinews.models import (VaderAnalyzer,
 
 articleSchema = ArticleSchema()
 articlesSchema = ArticleSchema(many=True)
+weeklySchema = WeeklySchema(many=True)
+
+scoresSchemas = ScoreSchema(many=True)
+
 load_dotenv()
 logging.basicConfig(level=logging.DEBUG)
 
@@ -24,10 +29,10 @@ DB_URL = os.environ['DB_URL']
 DB_PARSE_COLUMNS = os.environ['DB_PARSE_COLUMNS']
 DB_SELECT_COLUMNS = os.environ['DB_SELECT_COLUMNS']
 
-va = VaderAnalyzer()
-tb = TextBlobAnalyzer()
-lstm = LSTMAnalyzer()
-bert = BERTAnalyzer()
+# va = VaderAnalyzer()
+# tb = TextBlobAnalyzer()
+# lstm = LSTMAnalyzer()
+# bert = BERTAnalyzer()
 
 COLUMNS_TO_QUERY = [
     DBArticle.url,
@@ -44,7 +49,7 @@ COLUMNS_TO_QUERY = [
     DBArticle.bert_p_neg,
 ]
 
-DEFAULT_NUM_TO_QUERY = 100
+DEFAULT_NUM_TO_QUERY = 20
 
 
 # todo: add validation and verification for requests
@@ -53,6 +58,7 @@ DEFAULT_NUM_TO_QUERY = 100
 @app.route("/")
 def home():
     return render_template("home.html", title='home'), 200
+
 
 @app.route("/models")
 def models():
@@ -227,6 +233,16 @@ def candidate(name):
     return "No results found", 400
 
 
+@app.route('/everything/<candidate>')
+def everything(candidate):
+    if check_password(request.headers) is False:
+        return "Unauthorized", 401
+    else:
+        query = DBArticle.query
+
+        return articlesSchema.jsonify(query.filter(DBArticle.title.ilike(f"%{candidate}%")).all(), many=True)
+
+
 @app.route('/news/<name>', methods=['GET'])
 def news_co(name):
     """
@@ -269,14 +285,16 @@ def query_database(candidate, news, opinion, all=False):
     if news:
         query = query.filter(DBArticle.news_co.ilike(f"%{news}%"))
     if candidate:
-        query = query.filter(DBArticle.title.ilike(f"%{candidate}%"))
+        query = query.filter(DBArticle.candidate.ilike(f"%{candidate}%"))
     if opinion:
         query = query.filter(DBArticle.url.contains('opinion'))
     if all:
         results = query.all()
-    results = query.order_by(DBArticle.datetime.desc()) \
-        .with_entities(*COLUMNS_TO_QUERY) \
-        .limit(DEFAULT_NUM_TO_QUERY).all()
+
+    # .with_entities(*COLUMNS_TO_QUERY).limit(100). \
+    #         filter(DBArticle.datetime > dt.now(timezone.utc) - timedelta(days=90)).all()
+    results = query.order_by(DBArticle.datetime.desc()). \
+        limit(100).all()
     if results:
         results = results[::-1]
     return results
@@ -284,7 +302,7 @@ def query_database(candidate, news, opinion, all=False):
 
 def check_password(headers):
     pw = headers.get('password')
-    if pw != -1:
+    if pw:
         logging.info(pw + '\n' + os.environ['AUTH_PASSWORD'])
         logging.info(pw == os.environ['AUTH_PASSWORD'])
         return pw == os.environ['AUTH_PASSWORD']
@@ -324,6 +342,13 @@ def update_all_rows():
     return "Success", 200
 
 
+@app.route('/join')
+def join():
+    results = DBArticle.query. \
+        limit(10).all()
+    return articlesSchema.jsonify(results)
+
+
 def load_in_chunks():
     offset = 0
     chunk_size = 500
@@ -338,3 +363,149 @@ def load_in_chunks():
         else:
             break
         offset += chunk_size
+
+
+@app.route('/new_avg')
+def avg():
+    with profiled():
+        args = request.args.to_dict()
+
+        news_co = args.get('news_co', None)
+        candidate = args.get('candidate', None)
+
+        if news_co is None:
+            return "Please specify a news company", 400
+        if candidate is None:
+            return "Please specify a candidate", 400
+
+        today = dt.now(tz=timezone.utc)
+        past_limit = today - timedelta(days=90)
+        # Filter by date range, news group, and candidate
+        results = DBArticle.query. \
+            filter(DBArticle.datetime.between(past_limit, today)). \
+            filter(DBArticle.news_co.ilike(f"%{news_co}%")). \
+            filter(DBArticle.candidate.ilike(f"%{candidate}%")).all()
+
+        return articlesSchema.jsonify(results, many=True)
+
+
+@app.route('/avg_test')
+def avg_test():
+    return jsonify(average_by_dates('trump', 'cnn'))
+
+
+def average_by_dates(candidate, news_co):
+    """
+    # todo: add functionality to choose window size
+    Starting at a designated date, this function will step 1 week at a time and calculate
+    the average score for each model for the given candidate and news group.
+    The function looks half the window size backwards in time and half the window size forwards in time
+    to form the whole window that will be averaged.
+    :param candidate: Name of candidate to filter by, e.g. Warren
+    :type candidate:  str
+    :param news_co: Name of news group to filter by, e.g. CNN
+    :type news_co: str
+    :return: List of dictionaries with date, bert_avg, lstm_avg, vader_avg, and textblob_avg as keys
+    :rtype: list of dict
+    """
+    import time
+    starting_date = dt.fromisoformat("2019-01-01 00:00+00:00")
+    window_size = 3  # in weeks
+    time_diff = dt.now(tz=timezone.utc) - starting_date
+    num_weeks = time_diff / timedelta(days=7)
+    to_return = []
+
+    # focus_date is the date for the middle of the window
+    focus_date = starting_date
+
+    # Filter by date range, news group, and candidate
+    results = DBArticle.query. \
+        filter(DBArticle.news_co.ilike(f"%{news_co}%")). \
+        filter(DBArticle.candidate.ilike(f"%{candidate}%")).all()
+
+    t5 = time.perf_counter()
+    for i in range(round(num_weeks)):
+        t1 = time.perf_counter()
+        temp_start = focus_date - timedelta(weeks=window_size / 2)
+        temp_end = focus_date + timedelta(weeks=window_size / 2)
+
+        t2 = time.perf_counter()
+
+        b_sum, l_sum, v_sum, t_sum = 0, 0, 0, 0
+        num_samples = len(results)
+
+        for sample in results:
+            b_sum += sample.scores.bert
+            l_sum += sample.scores.lstm
+            v_sum += sample.scores.vader
+            t_sum += sample.scores.textblob
+
+        bert_avg = b_sum / num_samples
+        lstm_avg = l_sum / num_samples
+        vader_avg = v_sum / num_samples
+        textblob_avg = t_sum / num_samples
+        #
+        # # Calculate averages for each model type
+        # num_elements = len(results)
+        # bert_avg = round(sum([sample.scores.bert for sample in results])/num_elements, 3)
+        # lstm_avg = round(sum([sample.scores.lstm for sample in results])/num_elements, 3)
+        # vader_avg = round(sum([sample.scores.vader for sample in results])/num_elements, 3)
+        # textblob_avg = round(sum([sample.scores.textblob for sample in results])/num_elements, 3)
+
+        t3 = time.perf_counter()
+
+        to_return.append({
+            'date': focus_date,
+            'bert_avg': bert_avg,
+            'lstm_avg': lstm_avg,
+            'vader_avg': vader_avg,
+            'textblob_avg': textblob_avg,
+        })
+
+        focus_date += timedelta(weeks=1)
+
+        t4 = time.perf_counter()
+
+        print(f"1-2: {t2 - t1}", f"2-3: {t3 - t2}", f"3-4: {t4 - t3}", f"5-1: {t1 - t5}")
+
+        t5 = time.perf_counter()
+
+    return to_return
+
+
+@app.route('/averages')
+def lookup():
+
+    args = request.args.to_dict()
+    news = args.get('news_co', None)
+    candidate = args.get('candidate', None)
+
+    if news is None or candidate is None:
+        return "Please add candidate and news group", 400
+
+    results = Weekly.query. \
+        filter(Weekly.news_co.ilike(f"%{news}%")). \
+        filter(Weekly.candidate.ilike(f"%{candidate}%")). \
+        filter(Weekly.datetime > "2019-06-01").\
+        order_by(Weekly.datetime.asc()).all()
+    bert = []
+    lstm = []
+    vader = []
+    textblob = []
+    datetime = []
+    for row in results:
+        bert.append(row.bert)
+        lstm.append(row.lstm)
+        vader.append(row.vader)
+        textblob.append(row.textblob)
+        datetime.append(row.datetime)
+
+    return jsonify({
+        'bert': bert,
+        'lstm': lstm,
+        'vader': vader,
+        'textblob': textblob,
+        'datetime': datetime,
+    }), 200
+
+
