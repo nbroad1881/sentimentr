73622f9bfcaa4a5e5b91634131385c25d1b89685
Nicholas Broad <nbroad94@gmail.com>
2020-03-13 16:29:32 -0400

    feat: tabulator schema

--- flask_app/schemas.py
+++ flask_app/schemas.py
@@ -1,4 +1,4 @@
-from flask_app.db_models import DBArticle, DBScore, Weekly
+from flask_app.db_models import DBArticle, DBScore, Weekly, Tabulator
 from flask_app import ma
 from marshmallow import fields
 
@@ -12,28 +12,6 @@ class ScoreSchema(ma.SQLAlchemySchema):
     vader = fields.Float()
     textblob = fields.Float()
 
-    bert_14day_avg = fields.Float()
-    lstm_14day_avg = fields.Float()
-    vader_14day_avg = fields.Float()
-    textblob_14day_avg = fields.Float()
-
-
-class ArticleSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = DBArticle
-
-    url = ma.auto_field()
-    title = ma.auto_field()
-    datetime = ma.auto_field()
-    # candidate = ma.auto_field()
-    # news_co = ma.auto_field()
-
-    bert_14day_avg = ma.auto_field()
-    lstm_14day_avg = ma.auto_field()
-    vader_14day_avg = ma.auto_field()
-    textblob_14day_avg = ma.auto_field()
-
-    # scores = fields.Nested(ScoreSchema)
 
 class WeeklySchema(ma.SQLAlchemySchema):
     class Meta:
@@ -43,6 +21,23 @@ class WeeklySchema(ma.SQLAlchemySchema):
     candidate = ma.auto_field()
     news_co = ma.auto_field()
     datetime = ma.auto_field()
+
+    bert = ma.auto_field()
+    lstm = ma.auto_field()
+    vader = ma.auto_field()
+    textblob = ma.auto_field()
+
+
+class TabulatorSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Tabulator
+
+    title = ma.auto_field()
+    url = ma.auto_field()
+    candidate = ma.auto_field()
+    news_co = ma.auto_field()
+    datetime = ma.auto_field()
+
     bert = ma.auto_field()
     lstm = ma.auto_field()
     vader = ma.auto_field()
